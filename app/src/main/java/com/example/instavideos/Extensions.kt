4bdd75667edbe77e3a04a2131d945a61ed8d47aa
eyyubovnq <050629573741nN@>
2023-05-06 23:28:24 +0400

    added circular menu

--- app/src/main/java/com/example/instavideos/Extensions.kt
+++ app/src/main/java/com/example/instavideos/Extensions.kt
@@ -1,12 +1,70 @@
 package com.example.instavideos
 
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Resources
+import android.util.Log
+import android.view.View
+import android.view.ViewPropertyAnimator
+import android.view.animation.Animation
+import android.view.animation.BaseInterpolator
+import android.view.animation.BounceInterpolator
+import android.view.animation.Interpolator
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 
 
 fun Fragment.getColor(color: Int) = ContextCompat.getColor(requireContext(), color)
+fun Context.getColorContext(color: Int) = ContextCompat.getColor(this, color)
 
 val Int.toPx get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 val Int.toDp get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+fun ImageView.setTint(color: Int) {
+    this.imageTintList = ColorStateList.valueOf(this.context.getColorContext(R.color.black))
+}
+
+fun ViewPropertyAnimator.onAnimationFinished(callback: () -> Unit) {
+    this.setListener(object : Animator.AnimatorListener {
+        override fun onAnimationStart(animation: Animator) {}
+        override fun onAnimationEnd(animation: Animator) {
+            callback()
+        }
+
+        override fun onAnimationCancel(animation: Animator) {}
+        override fun onAnimationRepeat(animation: Animator) {}
+    })
+}
+
+fun View.animateWidthAndHeight(
+    size: Int,
+    duration: Long = 0,
+    interpolator: Interpolator? = null,
+    onFinish: (() -> Unit)? = null
+) {
+
+    val slideAnimator = ValueAnimator
+        .ofInt(this.width, size)
+        .setDuration(duration)
+
+    slideAnimator.addUpdateListener { animation1: ValueAnimator ->
+        val value = animation1.animatedValue as Int
+        this.layoutParams.width = value
+        this.layoutParams.height = value
+        this.requestLayout()
+        if (value == size) {
+            onFinish?.invoke()
+        }
+    }
+
+    slideAnimator.interpolator = interpolator
+    slideAnimator.start()
+
+
+}
+
---
+++ app/src/main/java/com/example/instavideos/fragments/CircularMenuFragment.kt
@@ -0,0 +1,203 @@
+package com.example.instavideos.fragments
+
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
+import android.animation.ValueAnimator
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AnticipateInterpolator
+import android.view.animation.BounceInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.OvershootInterpolator
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.instavideos.R
+import com.example.instavideos.animateWidthAndHeight
+import com.example.instavideos.databinding.FragmentCircularMenuBinding
+import com.example.instavideos.getColor
+import com.example.instavideos.onAnimationFinished
+import com.example.instavideos.setTint
+import com.example.instavideos.toPx
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class CircularMenuFragment : Fragment(R.layout.fragment_circular_menu) {
+
+    private lateinit var binding: FragmentCircularMenuBinding
+
+    private var menuOpened = false
+
+    private val icons: List<ImageView> by lazy {
+        listOf(binding.icon1, binding.icon2, binding.icon3, binding.icon4, binding.icon5, binding.icon6)
+    }
+
+    private val blurs: List<ImageView> by lazy {
+        listOf(binding.blur1, binding.blur2, binding.blur3, binding.blur4, binding.blur5, binding.blur6)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCircularMenuBinding.bind(view)
+
+        with(binding) {
+            buttonFirstInner.setOnClickListener {
+                val animator = ValueAnimator.ofFloat(1f, 0.95f)
+                animator.addUpdateListener { animator ->
+                    val value = animator.animatedValue as Float
+                    it.scaleX = value
+                    it.scaleY = value
+
+                    if (value == 0.95f) {
+                        animator.reverse()
+                    }
+                }
+                animator.duration = 200
+                animator.start()
+
+                if (menuOpened) {
+                    icons.forEach { icon ->
+                        icon.scaleX = 0f
+                        icon.scaleY = 0f
+                    }
+                    blurs.forEach {
+                        it.isVisible = false
+                    }
+                    icons.forEach {
+                        it.isEnabled = false
+                        it.isClickable = false
+                    }
+                    strokeCircle.alpha = 0f
+                    arrow.rotation = 0f
+                    ivArrow.setColorFilter(getColorForIndex(0))
+                    resetColors()
+
+
+                    cardView.animateWidthAndHeight(
+                        size = 110.toPx,
+                        duration = 200L,
+                        interpolator = AccelerateInterpolator(),
+                        onFinish = {
+                            buttonSecondInner.animateWidthAndHeight(
+                                size = 110.toPx,
+                                duration = 100,
+                                interpolator = BounceInterpolator()
+                            )
+                        }
+                    )
+                } else {
+
+                    cardView.animateWidthAndHeight(
+                        size = 240.toPx,
+                        duration = 300L,
+                        interpolator = DecelerateInterpolator(),
+                        onFinish = {
+                            strokeCircle.alpha = 0.6f
+                            strokeCircle.setColorFilter(getColorForIndex(0))
+                            buttonSecondInner.animateWidthAndHeight(
+                                size = 130.toPx,
+                                duration = 300,
+                                interpolator = OvershootInterpolator()
+                            )
+                            resetIconAndBlurState()
+                            lifecycleScope.launch {
+                                icons.forEach { icon ->
+                                    icon.animate().scaleX(1f).scaleY(1f).setDuration(100).start()
+                                    delay(50)
+                                }
+                            }
+                            icons.forEach {
+                                it.isEnabled = true
+                                it.isClickable = true
+                            }
+                            blurs.first().alpha = 0.6f
+
+                        }
+                    )
+                }
+                changeState()
+
+            }
+        }
+        initClickListeners()
+
+    }
+
+    private fun getColorForIndex(index: Int): Int {
+        return when (index) {
+            0 -> getColor(R.color.gradient_1)
+            1 -> getColor(R.color.gradient_2)
+            2 -> getColor(R.color.gradient_3)
+            3 -> getColor(R.color.gradient_4)
+            4 -> getColor(R.color.gradient_5)
+            5 -> getColor(R.color.gradient_6)
+            else -> getColor(R.color.gradient_1)
+        }
+    }
+
+
+    private fun initClickListeners() = with(binding) {
+        icons.forEach {
+            it.isEnabled = false
+            it.isClickable = false
+        }
+        icons.forEachIndexed { index, icon ->
+            icon.setOnClickListener {
+                val animation = arrow.animate()
+                    .setDuration(500)
+                    .rotation((index * 60).toFloat())
+                    .setInterpolator(OvershootInterpolator())
+
+                animation.setListener(object : AnimatorListener {
+                    override fun onAnimationStart(animation: Animator) {}
+                    override fun onAnimationEnd(animation: Animator) {
+                        icons.forEachIndexed { idx, ic ->
+                            ic.setColorFilter(getColor(R.color.gray))
+                        }
+                        icon.setColorFilter(getColor(R.color.black))
+                        blurs.forEachIndexed { blurIndex, blur ->
+                            blur.alpha = 0f
+                        }
+                        blurs[index].animate()
+                            .alpha(0.6f)
+                            .start()
+
+                        ivArrow.setColorFilter(getColorForIndex(index))
+                        strokeCircle.setColorFilter(getColorForIndex(index))
+
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {}
+
+                    override fun onAnimationRepeat(animation: Animator) {}
+
+                })
+                animation.start()
+
+            }
+        }
+    }
+
+    private fun resetColors() {
+        icons.forEachIndexed { index, icon ->
+            icon.setColorFilter(if (index == 0) getColor(R.color.black) else getColor(R.color.gray))
+        }
+    }
+
+    private fun resetIconAndBlurState() = with(binding) {
+        icon1.setTint(R.color.black)
+        blur1.isVisible = true
+    }
+
+    private fun changeState() {
+        this.menuOpened = !menuOpened
+    }
+}
