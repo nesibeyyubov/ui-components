bb1b220901f949c5de3b1404da08066482cb64cf
eyyubovnq <050629573741nN@>
2023-12-09 22:03:19 +0400

    finish three d images animation

--- app/src/main/java/com/example/instavideos/fragments/ThreeDImagesFragment.kt
+++ app/src/main/java/com/example/instavideos/fragments/ThreeDImagesFragment.kt
@@ -1,34 +1,13 @@
 package com.example.instavideos.fragments
 
-import android.content.Context
-import android.graphics.Canvas
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.instavideos.Component
-import com.example.instavideos.GenericListAdapter
 import com.example.instavideos.R
-import com.example.instavideos.components
-import com.example.instavideos.databinding.ComponentLayoutBinding
-import com.example.instavideos.databinding.FragmentHomeBinding
-import com.example.instavideos.databinding.FragmentHorizontalMovingChipsBinding
 import com.example.instavideos.databinding.FragmentThreeDImagesBinding
-import com.example.instavideos.databinding.ItemLayoutBinding
-import com.example.instavideos.toDp
 import com.example.instavideos.toPx
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.launch
 
 
